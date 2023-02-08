e536b3992a2ab471e9d931d8d607d1763b3c1357
BaironSandoval <baironalfonsosandovalnino@gmail.com>
2023-02-08 15:52:49 -0500

    hito 1 done

--- src/main.js
+++ src/main.js
@@ -1,4 +1,38 @@
-function seleccionRespuestas(){
-  let seleccionRespuesta = document.querySelector('seleccionarRespuesta1')
+function iniciarTrivia(){
+  let botonEnviar = document.getElementById('boton-enviar');
+  botonEnviar.addEventListener('click', seleccionarRespuesta);
 }
-seleccionRespuestas()
+
+
+
+function seleccionarRespuesta(){
+  let inputSofia = document.getElementById('sofia');
+  let inputTimbu = document.getElementById('timbu');
+  let inputSkopje = document.getElementById('skopje');
+  let inputBuenosAires = document.getElementById('buenos-Aires');
+  let inputBogota = document.getElementById('bogota');
+  let inputDusambe = document.getElementById('dusambe');
+  
+
+
+  if(inputSofia.checked || inputTimbu.checked || inputSkopje.checked){
+    let selectionEnviar = document.getElementById('mensaje');
+  selectionEnviar.style.display = 'block'
+  }
+  else if(inputBuenosAires.checked || inputBogota.checked || inputDusambe.checked){
+    let selectionEnviar = document.getElementById('mensaje');
+    selectionEnviar.style.display = 'block'
+  }
+  else{alert('hola')}
+  
+}
+
+window.addEventListener('load', iniciarTrivia)
+
+
+
+
+
+
+
+
